9012976806fa2deda363f9d31a05bf2895d26c80
Jonas Grumann <j.grumann@bitmama.it>
2011-10-24 17:32:14 +0200

    added some comments, removed the double render thing and added the wavelength slider

--- Controls.js
+++ Controls.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	var gui = new DAT.GUI({
-	    height : 5 * 32 - 1
+	    height : 6 * 32 - 1
 	});
 	gui.add(params, 'randomness').min(0).max(40).step(1)
 	gui.add(params, 'randsize').min(0).max(1000).step(1)
 	gui.add(params, 'camerapos').min(-500).max(-50).step(1)
 	gui.add(params, 'rotation').min(0).max(0.1).step(0.01)
+	gui.add(params, 'wavelength').min(3).max(40).step(1)
 	gui.add(params, 'emit')
 })
--- test_js.js
+++ test_js.js
@@ -2,19 +2,21 @@ var speeds = new Array();
 var xspeeds = new Array();
 var frame = 0
 var cubenum = new Array();
-var gravity=-9.81
-var times=new Array();
+var gravity = -9.81
+var times = new Array();
 var col = 0
 var randomness = 0.0;
 var randsize = 0;
 var camerapos = -300;
 var emit = true;
 var rotation = 0.0;
+var wavelength = 20.0;
 var params = {
 	randomness: 0,
 	randsize: 0,
 	camerapos: -300,
 	rotation: 0.0,
+	wavelength: 20,
 	emit: true
 };
 
@@ -38,7 +40,7 @@ var scene = new THREE.Scene();
 
 camera.position.z = camerapos;
 camera.position.x = -0;
-camera.position.y=0;
+camera.position.y = 0;
 
 // start the renderer
 renderer.setSize(WIDTH, HEIGHT);
@@ -71,7 +73,7 @@ var planeMaterial = new THREE.MeshPhongMaterial(
     color: '0xFFFFFF'
 });
 var plane = new THREE.Mesh(
-	new THREE.CubeGeometry(100,1,100,1), planeMaterial);
+	new THREE.CubeGeometry(100, 1, 100, 1), planeMaterial);
 var cube1 = new THREE.Mesh(
   new THREE.CubeGeometry(5, 2, 5, 1),
   sphereMaterial1);
@@ -90,66 +92,62 @@ function drawCube(){
 		if (params.emit == true){
 			col++;
 		var sphereMaterial = new THREE.MeshPhongMaterial(
-		{
-		    color: '0x'+ Math.floor((col%256-256)*(-1)).toString(16)+ Math.floor(col%128).toString(16)+ Math.floor(col%256).toString(16)
-			//color: '0xFFFFFF'
+		{color: '0x'+ Math.floor((col%256-256)*(-1)).toString(16)+ Math.floor(col%128).toString(16)+ Math.floor(col%256).toString(16)
 		});
 		var sphere = new THREE.Mesh(
 		  new THREE.CubeGeometry(1 + Math.random()*params.randsize / 100, 1 + Math.random()*randsize / 100, 1 + Math.random()*randsize / 100, 1),
 		  sphereMaterial);
 			sphere.overdraw = true;
-			sphere.position.x=0;
-			sphere.position.y=0;
-			sphere.position.z=0;
-			sphere.rotation.x=Math.random()+359;
-			sphere.rotation.y=Math.random()+359;
-			sphere.rotation.z=Math.random()+359;
+			sphere.position.x = 0;
+			sphere.position.y = 0;
+			sphere.position.z = 0;
+			sphere.rotation.x = Math.random() + 359;
+			sphere.rotation.y = Math.random() + 359;
+			sphere.rotation.z = Math.random() + 359;
 
 			// add the sphere to the scene
 			times.push(1);
-			//speeds.push(Math.random()*.5+5);
+			// update speeds array
 			speeds.push(5.5 + Math.random()*params.randomness / 100);
-			//speeds.push(5.0);
-
+			// push cube (erhm) into array
 			cubenum.push(sphere);
-
-			xspeeds.push(Math.sin(cubenum.length/20)*2);
+			// update xspeeds array
+			xspeeds.push(Math.sin(cubenum.length/params.wavelength)*2);
 			scene.addChild(sphere);
-			renderer.render(scene, camera);
-
 	}
+	
 }
 
 $('#container').mousemove(function(e){
-	
-		if(cubenum.length == 0){
-			animate();
+	// I want the animate() method to be triggered only once from here, not each time I move the mouse
+	if(cubenum.length == 0){
+		animate();
 	}
 	camera.position.y = e.clientY / 2 - 40;
 	camera.position.x = e.clientX / 4 - 200;
-	
-
 })
 
 function animate() {
-	//cube1.rotation.y+=0.02;
 	camera.position.z = params.camerapos;
 	drawCube();
 	for(q=0; q<cubenum.length;q++){
-	times[q] += 0.001;
-	//cubenum[q].position.x = cubenum[q].position.x / speed;
-	cubenum[q].position.y += speeds[q] * times[q] + 0.5 * (gravity) * times[q] * times[q];
-	cubenum[q].position.z = cubenum[q].position.z-0.8
-	cubenum[q].position.x += xspeeds[q]/10;
-	cubenum[q].rotation.y += params.rotation;
-	cubenum[q].rotation.x += params.rotation;
-	if (cubenum[q].position.y < (-20)){
-		scene.removeChild(cubenum[q])
+		times[q] += 0.001;
+		//cubenum[q].position.x = cubenum[q].position.x / speed;
+		cubenum[q].position.y += speeds[q] * times[q] + 0.5 * (gravity) * times[q] * times[q];
+		cubenum[q].position.z = cubenum[q].position.z - 0.8
+		cubenum[q].position.x += xspeeds[q] / 10;
+		cubenum[q].rotation.y += params.rotation;
+		cubenum[q].rotation.x += params.rotation;
+		// Remove cube if it goes down too deep
+		if (cubenum[q].position.y < (-20)){
+			scene.removeChild(cubenum[q]);
+		}
+		// Or up too high
+		if (cubenum[q].position.y > (140)){
+			scene.removeChild(cubenum[q])
+		}
 	}
-	if (cubenum[q].position.y > (140)){
-		scene.removeChild(cubenum[q])
-	}
-}
+	// Unleash the fury
 	renderer.render( scene, camera );
 	requestAnimFrame(animate);
 };
