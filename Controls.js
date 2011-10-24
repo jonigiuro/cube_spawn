9675cf2b824bd6e099ad2c5e6bb62f690baa00ba
Jonas Grumann <j.grumann@bitmama.it>
2011-10-24 17:56:54 +0200

    added formula switch sin_tan

--- Controls.js
+++ Controls.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 	var gui = new DAT.GUI({
-	    height : 6 * 32 - 1
+	    height : 7 * 32 - 1
 	});
 	gui.add(params, 'randomness').min(0).max(40).step(1)
 	gui.add(params, 'randsize').min(0).max(1000).step(1)
 	gui.add(params, 'camerapos').min(-500).max(-50).step(1)
 	gui.add(params, 'rotation').min(0).max(0.1).step(0.01)
 	gui.add(params, 'wavelength').min(3).max(40).step(1)
+	gui.add(params, 'sin_tan')
 	gui.add(params, 'emit')
 })
